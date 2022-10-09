cfb6137d5c193cda020e95811e3c19b73365d131
Lyndsey Reed <43240450+LReed2144@users.noreply.github.com>
2022-10-08 21:05:24 -0700

    create admin user

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,17 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Category(models.Model):
+    categoryName = models.CharField(max_length=50)
+#create listings model
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=640)
+    imageUrl = models.CharField(max_length=1000)
+    price = models.FloatField()
+    isActive = models.BooleanField(default=True)
+    #will delete the user if admin deletes the user.  owner is a foreign key
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="user")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True,  related_name="category")
